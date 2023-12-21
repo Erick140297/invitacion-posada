de65f3499a6cd27cbdc5505d7c400d3f5b6dd738
Erick140297 <erickmonterrubio67@outlook.com>
2023-12-21 04:15:44 -0600

    v 1.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,76 @@ import styled from "styled-components";
 
 const App = () => {
   return (
-    <Container>Invitación Posada</Container>
-  )
-}
+    <Container>
+      <Div>
+        <P>Te invitamos</P>
+        <H2>A nuestra Posada Navideña</H2>
+        <P>Por parte de la familia Colula Salinas</P>
+      </Div>
+      <Div2>
+        <P2>23 de Diciembre de 2023</P2>
+        <P2>7:00 p.m.</P2>
+        <P2>Cerrada Paseo de Otoño Mz. 6 Lt. 2, colonia San Juan Zapotla</P2>
+      </Div2>
+      <Button>Ver ubicación</Button>
+    </Container>
+  );
+};
 
-export default App
+export default App;
 
 const Container = styled.div`
-  background: red;
+  background-image: url(https://res.cloudinary.com/dnrcmjyu1/image/upload/v1703150970/Projects/Invitaciones/fondo_zgpzge.jpg);
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Div = styled.div`
+  font-family: "Sevillana", cursive;
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 40px;
+`;
+
+const Div2 = styled.div`
+  font-family: "Sevillana", cursive;
+  text-align: center;
+  margin-bottom: 40px;
+`;
+
+const P = styled.p`
+  font-size: 25px;
+  font-weight: bold;
+  color: #4438e4;
+  text-shadow: 2px 2px 4px white;
+`;
+
+const P2 = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+  color: #2364bf;
+  text-shadow: 2px 2px 4px white;
+`;
+
+const H2 = styled.h2`
+  font-family: "Dancing Script", cursive;
+  font-size: 35px;
+  color: #ca7e13;
+  text-shadow: 2px 2px 4px white;
+  margin:10px;
+`;
+
+const Button = styled.button`
+  padding:5px;
+  background: #ffffff;
+  font-family: "Dancing Script", cursive;
+  font-size: 20px;
+  color: #ca7e13;
+  border-radius: 5px;
+  border: 2px #ca7e13 solid;
 `
